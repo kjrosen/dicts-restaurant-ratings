9eb6352fe39cc911ef1c02ee2dfb72c71c5ed0a1
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-21 16:52:50 -0700

    Printed data alphabetically + Added restaurants

--- ratings.py
+++ ratings.py
@@ -8,16 +8,29 @@ restaurant_file = open(sys.argv[1])
 restaurants_data = {}
 for restaurant in restaurant_file:
     restaurant_rating = restaurant.split(":")
-    print(restaurant_rating)
+    # print(restaurant_rating)
 
     name, rating = restaurant_rating
     restaurants_data[name] = restaurants_data.get(name,rating)
-print(restaurants_data) 
+# print(restaurants_data) 
 
-for restaurant in restaurants_data:
-    sorted(restaurants_data.items())
+# sorted_restaurants = sorted(restaurants_data.items())
 
+for restaurant in sorted(restaurants_data):
+    print(restaurant, restaurants_data[restaurant])
 
+new_name = input("What is the restaurant name? ")
+
+while True:
+    rating = input("What is the score? ")
+    if int(rating) < 1 or int(rating) > 5:
+        print("This is invalid. The rate should be between 1 and 5. ")
+    else:
+        break
+
+restaurants_data[new_name] = rating
     
+for restaurant in sorted(restaurants_data):
+    print(restaurant, restaurants_data[restaurant])
 
 
